8d681340c2bcfec1a47d188cdd80b7c70ef75ce6
sathya943 <satyasai.datascience943@gmail.com>
2026-07-06 13:23:45 +0530

    Updates hello_world function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def hello_world():
-  return "Hello World!"
+def hello_world(name : str) -> str:
+  return f'Hello {name}!Welcome to python world'
 
 ## Driver Code
-print(hello_world())
+print(hello_world('Satya'))
