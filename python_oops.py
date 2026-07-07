d95120c714ac99345666e534b398e2aa3dbaea48
IsarapuSatyasai <esarapusathyasai603@gmail.com>
2026-07-07 10:24:59 +0530

    New 'Calculator' Class with Basic arithmetic methods created.

--- python_oops.py
+++ python_oops.py
@@ -8,6 +8,36 @@ class Person:
     def display(self):
         print(f"Name: {self.name}, Age: {self.age}")
 
+
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+    
+    def add(self):
+        return self.num1 + self.num2
+    
+    def subtract(self):
+        return self.num1 - self.num2
+    
+    def multiply(self):
+        return self.num1 * self.num2
+    
+    def divide(self):
+        try:
+            return self.num1 / self.num2
+        except ZeroDivisionError:
+            return "Cannot divide by zero"
+    
+    def display(self):
+        print(f"Addition: {self.add()}")
+        print(f"Subtraction: {self.subtract()}")
+        print(f"Multiplication: {self.multiply()}")
+        print(f"Division: {self.divide()}")
+
 # Driver Code
-person1 = Person("Satya", 22)
-person1.display()
+# person1 = Person("Satya", 22)
+# person1.display()
+
+calculator1 = Calculator(10, 5)
+calculator1.display()
