23a75dec21707d95353dc0fb3925f3d0b370c4c3
sathya943 <satyasai.datascience943@gmail.com>
2026-07-07 10:20:38 +0530

    Created Simple Python 'Person' Class program with Driver Code.

--- python_oops.py
+++ python_oops.py
@@ -1,2 +1,13 @@
 # Learn Python Object Oriented Programming Concepts Here.
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}")
+
+# Driver Code
+person1 = Person("Satya", 22)
+person1.display()
